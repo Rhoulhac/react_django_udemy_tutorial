116acbd7b4f141c4ac73ff81f4803975c9cb542d
Rhoulhac <greg.rhoulhac@gmail.com>
2022-06-18 00:46:16 -0500

    add new fields to book model

--- application/models.py
+++ application/models.py
@@ -7,3 +7,24 @@ class Book(models.Model):
         blank=False,
         unique=True,
     )
+    description = models.TextField(
+        blank=True,
+        max_length=256,
+    )
+    price = models.DecimalField(
+        blank=True,
+        decimal_places=2,
+        max_digits=3,
+        null=True,
+    )
+    published = models.DateField(
+        blank=True,
+        null=True,
+    )
+    is_published = models.BooleanField(
+        default=False,
+    )
+    cover = models.ImageField(
+        blank=True,
+        upload_to='covers/',
+    )
