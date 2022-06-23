99373dbe9a06301fcc135bee15d7d2cda09c92f5
Rhoulhac <greg.rhoulhac@gmail.com>
2022-06-22 22:47:56 -0500

    update book admin

--- application/admin.py
+++ application/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['title', 'published', 'price']
+    search_fields = ['title', 'description']
